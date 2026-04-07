336fc9e2343f67aa4bdbc21bff453a52d97185c6
ayushsingh160107 <jaivikasnagar1601@gmail.com>
2026-04-08 00:34:15 +0530

    first commit

---
+++ 04-card-projects/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Bookmark } from 'lucide-react'
+import Card from './components/Card.jsx'
+import './App.css'
+const App = () => {
+  return (
+    <div className='parent'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
