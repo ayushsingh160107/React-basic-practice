9182d75d9bddcf21e262de7cc725b346ac5455a3
ayushsingh160107 <jaivikasnagar1601@gmail.com>
2026-05-07 01:15:30 +0530

    Props Project

--- 04-card-projects/src/components/Card.jsx
+++ 04-card-projects/src/components/Card.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Bookmark } from 'lucide-react'
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <div className="card">
         <div className="top">
-          <img src="https://cdn.vectorstock.com/i/500p/39/87/amazon-logo-smile-icon-vector-34243987.jpg" alt="" />
+          <img src={props.logo} alt="" />
           <button>Save <Bookmark size={12} /></button>
         </div>
         <div className="centre">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
+          <h3>{props.company}<span>{props.date}</span></h3>
+          <h2>{props.post}</h2>
           <div className='tag'>
-            <h4>Part Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
         <div className="bottom">
           <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai, India</p>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
           </div>
           <button>Apply Now</button>
         </div>
